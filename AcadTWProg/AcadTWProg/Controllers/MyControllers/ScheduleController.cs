50c3a06ad214bdf1b7be1fd7b0ad27f0cd2ac7a1
Mohamad Kalakesh <mohamad.kalakesh.aspnet@outlook.com>
2020-06-02 19:30:32 +0300

    Adding ScheduleController and its Index

---
+++ AcadTWProg/AcadTWProg/Controllers/MyControllers/ScheduleController.cs
@@ -0,0 +1,29 @@
+﻿using AcadTWProg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcadTWProg.Controllers.MyControllers
+{
+    public class ScheduleController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ScheduleController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
